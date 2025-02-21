6d2fd73d9d9ca7165ccb823c1028ae79703f84af
Wejas-exe <tejassomani1134@gmail.com>
2025-02-21 19:03:57 +0530

    Time: 1 ms (99.82%), Space: 46.3 MB (6.93%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        HashSet<Integer> set = new HashSet<>();
-        int[] res = new int[2];
-        Arrays.sort(nums);
-        for(int i =0;i<n-1;i++){
-            set.add(nums[i]);
-            if(nums[i] == nums[i+1]){
-                res[0] = nums[i]; 
-            }
+        int[] count = new int[n + 1];
+        int duplicate = -1, missing = -1;
+        
+        for (int num : nums) {
+            count[num]++;
         }
-        set.add(nums[n-1]);
-        for(int i =1;i<=n;i++){
-            if(!set.contains(i)){
-                res[1] = i;
-            }
+        
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 2) duplicate = i;
+            if (count[i] == 0) missing = i;
         }
-        return res;
+        
+        return new int[]{duplicate, missing};
     }
 }
