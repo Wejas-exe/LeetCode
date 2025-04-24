2486d701d466ad90a58a84169bef0e41ba4927ab
Wejas-exe <tejassomani1134@gmail.com>
2025-04-24 23:44:19 +0530

    Time: 8 ms (57.96%), Space: 44.9 MB (78.62%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -5,16 +5,32 @@ class Solution {
             set.add(num);
         }
         int count = 0;
-        for(int i =0;i<nums.length;i++){
-            HashSet<Integer> set1 = new HashSet<>();
-            for(int j=i;j<nums.length;j++){
-                set1.add(nums[j]);
-                if(set1.size()==set.size()){
-                    count += nums.length-j;
-                    break;
+        int i =0;
+        int j =0;
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while(i<n){
+            map.put(nums[i] , map.getOrDefault(nums[i] ,0) +1);
+            while(j<nums.length && map.size() == set.size()){
+                count += (n-i) ;
+                map.put(nums[j] , map.get(nums[j])-1);
+                if(map.get(nums[j]) == 0){
+                    map.remove(nums[j]);
                 }
+                j++;
             }
+            i++;
         }
+        // for(int i =0;i<nums.length;i++){
+        //     HashSet<Integer> set1 = new HashSet<>();
+        //     for(int j=i;j<nums.length;j++){
+        //         set1.add(nums[j]);
+        //         if(set1.size()==set.size()){
+        //             count += nums.length-j;
+        //             break;
+        //         }
+        //     }
+        // }
         return count;
     }
 }
