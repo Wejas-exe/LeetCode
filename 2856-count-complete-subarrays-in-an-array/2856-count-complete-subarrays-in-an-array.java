153262a1e8c6a6c29258a3961ef8a50b67120e28
Wejas-exe <tejassomani1134@gmail.com>
2025-04-24 23:35:55 +0530

    Time: 219 ms (22.09%), Space: 44.8 MB (90.26%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.java
@@ -4,13 +4,15 @@ class Solution {
         for(int num : nums){
             set.add(num);
         }
-        int size = set.size();
         int count = 0;
         for(int i =0;i<nums.length;i++){
             HashSet<Integer> set1 = new HashSet<>();
             for(int j=i;j<nums.length;j++){
                 set1.add(nums[j]);
-                if(set1.size()==size) count++;
+                if(set1.size()==set.size()){
+                    count += nums.length-j;
+                    break;
+                }
             }
         }
         return count;
