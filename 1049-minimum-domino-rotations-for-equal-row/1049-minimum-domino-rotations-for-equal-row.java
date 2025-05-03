8ec5966a3d82d27453108ded0e33a9b9ea7ed3b7
Wejas-exe <tejassomani1134@gmail.com>
2025-05-03 16:31:15 +0530

    Time: 4 ms (84.8%), Space: 50.3 MB (76%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,28 +1,24 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashMap<Integer,HashSet<Integer>> map = new HashMap<>();
-        int n = tops.length;
-        for(int i =0;i<n;i++){
-            map.computeIfAbsent(tops[i], k-> new HashSet<>()).add(i);
-            map.computeIfAbsent(bottoms[i], k-> new HashSet<>()).add(i);
-        }
-        int number = -1;
-        for(Map.Entry<Integer,HashSet<Integer>> entry : map.entrySet()){
-            if(entry.getValue().size() == n){
-                number = entry.getKey();
+        int top = tops[0];
+        int bottom = bottoms[0];
+        int ans = check(tops ,bottoms , top);
+        int res = check(tops , bottoms, bottom);
+        if(ans == -1 && res== -1) return -1;
+        if(ans == -1 ) return res ;
+        if(res == -1 ) return ans ;
+        return Math.min(ans , res);
+    }
+    public int check(int[] tops , int[] bottoms , int top){
+        int rotateTop = 0;
+        int rotateBottom = 0;
+        for(int i =0;i<tops.length;i++){
+            if(tops[i]!=top && bottoms[i]!=top){
+                return -1 ;
             }
+            if(tops[i]!= top) rotateTop++;
+            if(bottoms[i] != top) rotateBottom++;
         }
-        if(number == -1) return -1;
-        int freq = 0;
-        int count = 0;
-        for(int i =0;i<n;i++){
-            if(tops[i]==number){
-                freq++;
-            }
-            if(bottoms[i]==number){
-                count++;
-            }
-        }
-        return Math.min(Math.min(freq,n-freq),Math.min(count ,n-count ));
+        return Math.min(rotateTop , rotateBottom);
     }
 }
