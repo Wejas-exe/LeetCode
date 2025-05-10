33430f4ff3368a68df3e869880c7d2a52bfec8bc
Wejas-exe <tejassomani1134@gmail.com>
2025-05-10 17:17:18 +0530

    Time: 3 ms (88.73%), Space: 56.6 MB (99.53%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,18 +1,11 @@
 class Solution {
     public long minSum(int[] nums1, int[] nums2) {
-        long sum1 = 0;
-        long count1 = 0;
-        long sum2 = 0;
-        long count2 = 0;
-        for(int num : nums1){
-            sum1 += num;
-            if(num == 0) count1++;
-        }
-        for(int num : nums2){
-            sum2 += num;
-            if(num == 0) count2++;
-        }
-        System.out.println(sum1 + " " + sum2);
+        long[] res1 = helper(nums1);
+        long[] res2 = helper(nums2);
+
+        long sum1 = res1[0], count1 = res1[1];
+        long sum2 = res2[0], count2 = res2[1];
+
         if(sum1 == sum2){
             if(count2 == 0 && count1 == 0) return sum1;
             if(count1 == 0 || count2 == 0) return -1;
@@ -27,5 +20,14 @@ class Solution {
             return Math.max(sum2 + count2, sum1 + count1);
         }
         return -1;
+    }
+
+    public long[] helper(int[] arr){
+        long sum = 0, count = 0;
+        for(int num : arr){
+            sum += num;
+            if(num == 0) count++;
+        }
+        return new long[]{sum, count};
     }
 }
