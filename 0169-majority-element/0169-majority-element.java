02da07d7070061c16bb0a90bd116d5b66ee79920
Wejas-exe <tejassomani1134@gmail.com>
2025-05-11 16:58:54 +0530

    Time: 15 ms (22%), Space: 49.4 MB (86.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,9 +3,6 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int num : nums){
             map.put(num , map.getOrDefault(num , 0)+1);
-        }
-        Set<Integer> set = map.keySet();
-        for(int num : set){
             if(map.get(num)*2 >= nums.length) return num ;
         }
         return -1 ;
