1fa28770531884e420ed0bb15a0a527b3a1ba03a
Wejas-exe <tejassomani1134@gmail.com>
2025-05-11 17:11:37 +0530

    Time: 20 ms (6.18%), Space: 49.4 MB (8.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         HashMap<Integer,Integer> map = new HashMap<>();
         double val = Math.floor(nums.length/3);
         for(int num : nums){
             map.put(num , map.getOrDefault(num , 0)+1);
             if(map.get(num) > val ) {
-                if(!list.contains(num)){
-                    list.add(num);
-                }
+                set.add(num);
             }
         }
+        ArrayList<Integer> list = new ArrayList<>(set);
         return list ;
     }
 }
