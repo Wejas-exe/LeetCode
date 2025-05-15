70525deca5eb6082e6f4962d25efb1b7b31d07b2
Wejas-exe <tejassomani1134@gmail.com>
2025-05-15 12:57:14 +0530

    Time: 1 ms (96.15%), Space: 44.8 MB (89.07%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.java
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.java
@@ -1,38 +1,22 @@
 class Solution {
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
         int n = words.length;
-        if(n==1){
-            List<String> list3 = new ArrayList<>();
-            list3.add(words[0]);
-            return list3;
-        }
-        int prev1 = groups[0];
-        list1.add(0);
-        int prev2 = groups[1];
-        list2.add(1);
-        for(int i =1;i<n;i++){
-            if(groups[i] != prev1){
-                list1.add(i);
-                prev1 = groups[i];
-            }
-            if(groups[i] != prev2){
-                list2.add(i);
-                prev2 = groups[i];
-            }
-        }
-        if(list1.size() > list2.size()){
-            return helper(words,list1);
-        }else{
-            return helper(words,list2);
-        }
+        if (n == 1) return new ArrayList<>(List.of(words[0]));
+        List<String> list1 = helper(words , groups , 0);
+        List<String> list2 = helper(words , groups , 1);
+        if(list1.size() > list2.size()) return list1;
+        return list2;     
     }
-    public List<String> helper(String[] words  , List<Integer> list){
-        List<String> list1 = new ArrayList<>();
-        for(int i =0;i<list.size();i++){
-            list1.add(words[list.get(i)]);
+    public List<String> helper(String[] words  , int[] groups , int prev){
+        List<String> list = new ArrayList<>();
+        list.add(words[prev]);
+        int compare = groups[prev];        
+        for(int i =prev+1;i<words.length;i++){
+            if(groups[i] != compare){
+                list.add(words[i]);
+                compare = groups[i];
+            }
         }
-        return list1;
+        return list;
     }
 }
