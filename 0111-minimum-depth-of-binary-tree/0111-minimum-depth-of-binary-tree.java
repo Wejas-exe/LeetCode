76036e8e59d7ae057357f122873019f0e81054c9
Wejas-exe <tejassomani1134@gmail.com>
2025-06-06 10:41:22 +0530

    Time: 5 ms (47.76%), Space: 63 MB (87.41%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -16,14 +16,17 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null){
-            return 0;
+            return 0 ;
         }
-        if(root.left == null){
+        if(root.left == null && root.right == null){
+            return 1 ;
+        }else if(root.left == null){
             return minDepth(root.right) + 1;
+        }else if(root.right == null) {
+            return minDepth(root.left) + 1 ;
         }
-        if(root.right == null){
-            return minDepth(root.left) +1 ;
-        }
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return Math.min(left , right) +1 ;
     }
 }
