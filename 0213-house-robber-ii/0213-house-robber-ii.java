8a8f01f322771142b4b584e998ff4ffcbcfea718
Wejas-exe <tejassomani1134@gmail.com>
2025-06-16 19:54:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (18.57%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
-        if(nums.length==2) return Math.max(nums[0],nums[1]);
+        int n = nums.length;
+        if(n==1 ) return nums[0] ;
 
-        int pre = 0;
-        int prev = nums[0];
-        int max1 = 0;
-        for(int i =1;i<nums.length-1;i++){
-            max1 = Math.max(pre+nums[i],prev);
+        return Math.max(robber(nums , 1 , n-1) , robber(nums , 0 , n-2));
+    }
+    public int robber(int[] nums , int left , int right){
+        if(right == left){
+            return nums[left];
+        }
+        int pre = nums[left];
+        int prev = Math.max(nums[left] , nums[left+1]);
+        for(int i =left+2 ;i<=right;i++){
+            int curr = Math.max(pre+nums[i], prev);
             pre = prev ;
-            prev = max1;
+            prev = curr;
         }
-        pre = 0;
-        prev = nums[1];
-        int max2 = 0;
-        for(int i=2;i<nums.length;i++){
-            max2= Math.max(pre+nums[i],prev);
-            pre = prev;
-            prev = max2;
-        }
-        return Math.max(max1,max2) ;
+        return prev;
     }
 }
