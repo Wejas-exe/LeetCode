2a0731a3da8ea3591e47e6c20a1a734f493c74c3
Wejas-exe <tejassomani1134@gmail.com>
2025-06-16 18:48:41 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        int[] dp = new int[n+1];
         return climb(n , dp);
     }
     public int climb(int n , int[] dp){
-        if(n==1 || n==2 || n==3){
+        if(n == 1 || n == 2){
             return n ;
         }
         if(dp[n] != 0){
-            return dp[n];
+            return dp[n] ;
         }
-        dp[n-1] = climb(n-1,dp) ;
-        dp[n-2] = climb(n-2,dp) ;
-        return dp[n-1] +dp[n-2];
+        return dp[n] = climb(n-1 , dp) + climb(n-2 , dp);
     }
 }
