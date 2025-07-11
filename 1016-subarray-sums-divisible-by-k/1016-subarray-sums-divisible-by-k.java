6a3f1933d157ae53424f2744d636e1407f83a664
Wejas-exe <tejassomani1134@gmail.com>
2025-07-11 10:32:36 +0530

    Time: 3 ms (99.2%), Space: 46.1 MB (86.24%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,17 +1,16 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int sum = 0;
-        int ans = 0;
-        int n = nums.length;
         int[] count = new int[k];
         count[0] = 1;
-        for(int i =0;i<n;i++){
+        int sum = 0 ;
+        int ans = 0 ;
+        for(int i =0;i<nums.length;i++){
             sum += nums[i];
-            int rem = (sum%k);
-            if(rem < 0) rem+=k;
+            int rem = sum%k;
+            if(rem < 0) rem += k;
             ans += count[rem];
             count[rem]++;
-        }
-        return ans;
+        }   
+        return ans ;    
     }
 }
