c63e00be106ac5bb4ef6919092e05b5c21d4c7bb
Wejas-exe <tejassomani1134@gmail.com>
2025-07-25 08:53:19 +0530

    Time: 2 ms (74.06%), Space: 42.3 MB (78.44%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.java
@@ -1,23 +1,16 @@
 class Solution {
     public int maxSum(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         HashSet<Integer> set = new HashSet<>();
-        
-        for(int num: nums){
-            pq.add(num);
-        }
-        int max = pq.peek();
-        while(!pq.isEmpty()){
-            if(pq.peek() > 0){
-                set.add(pq.peek());
-            }
-            pq.poll();
-        }
         int sum = 0 ;
-        for(int n : set){
-            sum += n;
+        int max = Integer.MIN_VALUE;
+        for(int num:  nums){
+            max = Math.max(max,num);
+            if(num > 0 && !set.contains(num)){
+                sum += num;
+                set.add(num);
+            }
         }
-        if(set.size() == 0){
+        if(set.size() == 0 ){
             return max;
         }
         return sum;
