e55100046d6f5f0b41ee2f57b288062d329eadee
Wejas-exe <tejassomani1134@gmail.com>
2025-07-26 10:17:20 +0530

    Time: 13 ms (84.89%), Space: 47 MB (80.48%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,42 +2,32 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-
-        int[] dx = {0,0,1,-1};
-        int[] dy = {1,-1,0,0};
-
-        int[][] res = new int[m][n];
-
+        int[][] ans = new int[m][n];
         Queue<int[]> q = new LinkedList<>();
-
         for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(mat[i][j]==0){
-                    res[i][j]=0;
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    ans[i][j] = 0;
                     q.add(new int[]{i,j});
                 }else{
-                    res[i][j] =-1;
+                    ans[i][j] = -1;
                 }
             }
         }
-
-        int height = 1;
-
+        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
         while(!q.isEmpty()){
-            int size = q.size();
-            for(int i =0;i<size;i++){
-                int[] currCell = q.poll();
-                for(int d=0;d<4;d++){
-                    int nx = currCell[0] +dx[d];
-                    int ny = currCell[1] +dy[d];
-                    if(nx>=0 && nx<m && ny>=0 && ny<n && res[nx][ny]== -1){
-                        res[nx][ny] = height;
-                        q.offer(new int[]{nx,ny});
-                    }
+            int curr[] = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            for(int[] d: dir){
+                int nx = x+d[0];
+                int ny = y+d[1];
+                if(nx >= 0 && nx<m && ny>=0 && ny<n && ans[nx][ny] == -1){
+                    ans[nx][ny] = ans[x][y] +1;
+                    q.add(new int[]{nx,ny});
                 }
             }
-            height++;
         }
-        return res;
+        return ans;
     }
 }
