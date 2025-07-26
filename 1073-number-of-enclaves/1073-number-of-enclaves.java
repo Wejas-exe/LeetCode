f9f70d61c36b79e28133af9e4b7ddb8484ead206
Wejas-exe <tejassomani1134@gmail.com>
2025-07-26 17:10:32 +0530

    Time: 6 ms (98.6%), Space: 56.9 MB (49.92%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.java
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -2,38 +2,33 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        Queue<int[]> q = new LinkedList<>();
-        int land = 0 ;
         for(int i =0;i<m;i++){
+            dfs(grid,i,n-1,m,n);
+            dfs(grid,i,0,m,n);
+        }
+        for(int i=0;i<n;i++){
+            dfs(grid, 0,i,m,n);
+            dfs(grid, m-1,i,m,n);
+        }
+        int res = 0 ;
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0 || i==m-1 || j== n-1){
-                    if(grid[i][j] == 1){
-                        //land++;
-                        q.add(new int[]{i,j});
-                        visited[i][j] = true;
-                    }
-                }else if(grid[i][j] == 1){
-                    land++;
-                }
+                if(grid[i][j] == 1) res++;
             }
         }
-        int occ = 0 ;
-        int[][] dir = {{1,0},{0,1},{0,-1},{-1,0}};
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int x = curr[0];
-            int y = curr[1];
-            for(int[] d: dir){
-                int nx = x+d[0];
-                int ny = y+d[1];
-                if(nx >0 && nx<m-1 && ny>0 && ny<n-1 && grid[nx][ny] == 1 && !visited[nx][ny]){
-                    occ++;
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx,ny});
-                }
-            }
+        return res;
+
+    }
+    public static void dfs(int[][] grid, int i, int j, int m, int n){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
+            return ;
         }
-        return land-occ;
+
+        grid[i][j] = 2;
+
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i,j+1,m,n);
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i,j-1,m,n);
     }
 }
